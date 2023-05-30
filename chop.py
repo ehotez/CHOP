0a385d5844ad806f4a11cbd34ce46627b3fd1829
voin508 <nosirew.costya2016@yandex.ru>
2023-05-30 18:19:54 +0300

    Claim

--- chop.py
+++ chop.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from tkcalendar import Calendar
 import pyodbc
 import customtkinter as ct
 from tkinter.messagebox import NO
 from tkinter import CENTER, ttk
 
 # Установление соединения с базой данной HI HI HA HA
-conn = pyodbc.connect(driver = '{SQL Server}',server = 'EHOTEZPC' , database = 'dbCHOP', user = 'sa', password = 'sa')
+conn = pyodbc.connect(driver = '{SQL Server}',server = 'MSI' , database = 'dbCHOP', user = 'sa', password = 'sa')
 cur = conn.cursor()
 
 ct.set_appearance_mode('dark')
@@ -113,6 +114,92 @@ def login_frame():
     ct.CTkButton(login, text='Регистрация', command=register).grid(column=1,row=2)
 
 def main_frame(client_id, client_name):
+
+    def claim(client_id, client_name):
+        
+
+        def form_add_claim():
+
+            def add_claim1(name, flag, date):  
+                
+                if(name == ''or flag == '' or date == '' or (flag !=1 and flag!=0)):
+                    return
+                else:
+                    try:
+                        cur.execute('INSERT INTO tblClaim VALUES (?,?,?,?)', name, flag, date, client_id)
+                        conn.commit()
+                        add_claim.destroy()
+
+                        table.delete(*table.get_children())
+                        cur.execute('select txtClaimDescprition, isClaimClosed, dateClaimStart from tblClaim where (intClientId=?)', client_id)
+                        i=1 
+                        while(1): 
+                            result = str(cur.fetchone()) 
+                            if(result == "None"): 
+                                break 
+                            j = 0 
+                            while(j != len(result)): 
+                                if(result[j] == ' ' and result[j-1] != ','): 
+                                    result = result[:j] + '_' + result[j + 1:] 
+                                j+=1 
+                            replaced = result.replace(')','').replace('(','').replace('\'','').replace(',','') 
+                            table.insert(parent='',index='end',iid=i, values=replaced) 
+                            i+=1 
+                    except:
+                        print('error_claim')
+
+            add_claim = ct.CTkToplevel()
+            add_claim.grab_set()
+            add_claim.title('Добавить претензию')
+            add_claim.geometry('500x500')
+
+           
+
+            ct.CTkLabel(add_claim, text='Наименование претензии:').grid(column=0,row=0)
+            name_claim = ct.CTkEntry(add_claim)
+            name_claim.grid(column=0,row=1)
+            ct.CTkLabel(add_claim, text='Флаг закрытия:').grid(column=0,row=2)
+            flag_claim = ct.CTkEntry(add_claim)
+            flag_claim.grid(column=0,row=3)
+            ct.CTkLabel(add_claim, text='Дата претензии:').grid(column=0,row=4)
+            date_claim = Calendar(add_claim, selectmode = 'day')
+            date_claim.grid(column=0,row=5)
+
+            ct.CTkButton(add_claim, text='Добавить', command=
+                              lambda: add_claim1(name_claim.get(), flag_claim.get(), date_claim.get_date())).grid(column=0,row=8)
+
+            
+
+
+        cur.execute('select txtClaimDescprition, isClaimClosed, dateClaimStart from tblClaim where (intClientId=?)', client_id)
+        claim = ct.CTkToplevel()
+        claim.grab_set()
+        claim.title('Претензии клиента: '+ client_name)
+        claim.geometry('900x500')
+        table = ttk.Treeview(claim,columns=('Desceiption', 'Flag', 'Date'))
+        table.column('#0', width=0, stretch=NO)
+        
+        table.heading('Desceiption', text='Наименование претензии', anchor=CENTER)
+        table.heading('Flag', text='Флаг закрытия', anchor=CENTER)
+        table.heading('Date', text='Дата', anchor=CENTER)
+        
+        i=1
+        while(1):
+            result = str(cur.fetchone())
+            if(result == "None"):
+                break
+            j = 0
+            while(j != len(result)):
+                if(result[j] == ' ' and result[j-1] != ','):
+                    result = result[:j] + '_' + result[j + 1:]
+                j+=1
+            replaced = result.replace(')','').replace('(','').replace('\'','').replace(',','')
+            table.insert(parent='',index='end',iid=i, values=replaced)
+            i+=1
+        table.pack()
+        ct.CTkButton(claim, text='Добавить претензию', 
+                    command= form_add_claim).pack()
+
     
     def log_off():
         main.pack_forget()
@@ -125,7 +212,7 @@ def main_frame(client_id, client_name):
     ct.CTkButton(main, text='Список договоров', command=log_off).grid(column=0,row=0)
     ct.CTkButton(main, text='Новый договор', command=log_off).grid(column=0,row=1)
     ct.CTkButton(main, text='Распечатать договор', command=log_off).grid(column=0,row=2)
-    ct.CTkButton(main, text='Список претензий', command=log_off).grid(column=1,row=0)
+    ct.CTkButton(main, text='Список претензий', command= lambda: claim(client_id, client_name)).grid(column=1,row=0)
     ct.CTkButton(main, text='Новая претензия', command=log_off).grid(column=1,row=1)
     ct.CTkButton(main, text='Привязать сотрудника к договору', command=log_off).grid(columnspan=2,row=3)
     
